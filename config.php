0da902b0ef30b175bf2eb138c187db872a2e901e
Adarsh kumar singh <156177421+GeeksAdarsh@users.noreply.github.com>
2025-11-21 18:53:36 +0530

    Remove Google Client ID and Secret from config
    Removed Google OAuth credentials from config.php.

--- config.php
+++ config.php
@@ -2,8 +2,8 @@
 // Google OAuth Configuration
 // Get these from: https://console.cloud.google.com/apis/credentials
 
-define('GOOGLE_CLIENT_ID', '92025582338-21sumkqraqd9vo4gitbdsdp1agi6omj8.apps.googleusercontent.com');
-define('GOOGLE_CLIENT_SECRET', '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');
+define('');
+define('GOOGLE_CLIENT_SECRET', '');
 
 // Note: Client Secret is optional for JavaScript-based Google Sign-In
 // It's only needed if you want to verify tokens server-side (more secure)
